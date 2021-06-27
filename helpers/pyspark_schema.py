1212d49486c1ae36a7574e13bd6673425b1cb811
Christopher Chalcraft <chris.chalcraft@gmail.com>
2021-06-27 00:33:03 -0700

    yew

---
+++ helpers/pyspark_schema.py
@@ -0,0 +1,98 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC - Reference: http://nadbordrozd.github.io/blog/2016/05/22/one-weird-trick-that-will-fix-your-pyspark-schemas/
+# MAGIC - plan is to fix reading schemas
+
+# COMMAND ----------
+
+import pyspark.sql.types as pst
+from pyspark.sql import Row
+
+def infer_schema(rec):
+    """infers dataframe schema for a record. Assumes every dict is a Struct, not a Map"""
+    if isinstance(rec, dict):
+        return pst.StructType([pst.StructField(key, infer_schema(value), True)
+                              for key, value in sorted(rec.items())])
+    elif isinstance(rec, list):
+        if len(rec) == 0:
+            raise ValueError("can't infer type of an empty list")
+        elem_type = infer_schema(rec[0])
+        for elem in rec:
+            this_type = infer_schema(elem)
+            if elem_type != this_type:
+                raise ValueError("can't infer type of a list with inconsistent elem types")
+        return pst.ArrayType(elem_type)
+    else:
+        return pst._infer_type(rec)
+
+
+# COMMAND ----------
+
+def _rowify(x, prototype):
+    """creates a Row object conforming to a schema as specified by a dict"""
+
+    def _equivalent_types(x, y):
+        if type(x) in [str, unicode] and type(y) in [str, unicode]:
+            return True
+        return isinstance(x, type(y)) or isinstance(y, type(x))
+
+    if x is None:
+        return None
+    elif isinstance(prototype, dict):
+        if type(x) != dict:
+            raise ValueError("expected dict, got %s instead" % type(x))
+        rowified_dict = {}
+        for key, val in x.items():
+            if key not in prototype:
+                raise ValueError("got unexpected field %s" % key)
+            rowified_dict[key] = _rowify(val, prototype[key])
+            for key in prototype:
+                if key not in x:
+                    raise ValueError(
+                        "expected %s field but didn't find it" % key)
+        return Row(**rowified_dict)
+    elif isinstance(prototype, list):
+        if type(x) != list:
+            raise ValueError("expected list, got %s instead" % type(x))
+        return [_rowify(e, prototype[0]) for e in x]
+    else:
+        if not _equivalent_types(x, prototype):
+            raise ValueError("expected %s, got %s instead" %
+                             (type(prototype), type(x)))
+        return x
+
+
+# COMMAND ----------
+
+def df_from_rdd(rdd, prototype, sql):
+    """creates a dataframe out of an rdd of dicts, with schema inferred from a prototype record"""
+    schema = infer_schema(prototype)
+    row_rdd = rdd.map(lambda x: _rowify(x, prototype))
+    return sql.createDataFrame(row_rdd, schema)
+
+
+# COMMAND ----------
+
+dataPath = "/databricks-datasets/Rdatasets/data-001/csv/ggplot2/diamonds.csv"
+df = sqlContext.read.format("com.databricks.spark.csv")\
+  .option("header","true")\
+  .option("inferSchema", "true")\
+  .load(dataPath)
+
+# COMMAND ----------
+
+df.printSchema()
+
+# COMMAND ----------
+
+rdd = df.rdd.map(lambda x: x.asDict())
+diamonds = rdd.first()
+print(diamonds)
+
+# COMMAND ----------
+
+result = df_from_rdd(rdd, diamonds, sqlContext)
+
+# COMMAND ----------
+
+result.printSchema()
